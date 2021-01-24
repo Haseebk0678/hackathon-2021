277eca4b8b1754642a5882724e638177c012041f
Ryan Chen <ryanchen@Ryans-Air.home>
2021-01-24 00:59:21 -0500

    route issues fix

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['POST', 'GET'])
+@app.route('/', methods = ['POST', 'GET'])
 def home():
     return render_template('index.html')
 
-    
-@app.route("/result", methods = ['POST','GET'])
+
+@app.route('/result', methods = ['POST','GET'])
 def res():
   if request.method == 'POST':
     s = request.form['stateName']
-    return render_template("result.html",s = s)
-
-
-
+    return render_template('result.html',s = s)
